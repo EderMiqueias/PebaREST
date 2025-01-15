b13fa860e7df633ba036f420d6dc5b29b753bab6
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2025-01-15 16:51:01 -0300

    fix: simple_server superclass

--- tests/simple_server.py
+++ tests/simple_server.py
@@ -1,21 +1,21 @@
 from pebarest import App
+from pebarest.models import Resource
 
 from wsgiref.simple_server import make_server
 
 
 # Class for simple resource
-class GreetingResource:
-
-    def get(self, request):
+class GreetingResource(Resource):
+    def get(self, request, *args, **kwargs):
         return {"message": "Hello my little peba!"}
 
 
-app = App()
+app = App(default_headers={'Content-Type': 'application/json'})
 
-# Add a neu route path
+# Add a new route path
 app.add_route('/greeting', GreetingResource())
 
-# start the server
+# Start the server
 with make_server('', 8000, app) as httpd:
     print("Server listening on http://127.0.0.1:8000")
     httpd.serve_forever()
