7ecf66a97573ca9a5d03b23ca9c7d85be0c96941
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2025-01-15 10:47:33 -0300

    feat: implements __call__ function on Resource class

--- pebarest/models/resource.py
+++ pebarest/models/resource.py
@@ -6,8 +6,9 @@ from pebarest.models.http import HttpMethods
 
 class Resource:
     _map_methods: dict
+    headers: dict
 
-    def __init__(self):
+    def __init__(self, default_headers: dict=None):
         self._map_methods = {
             'GET': self.get,
             'POST': self.post,
@@ -17,9 +18,15 @@ class Resource:
             'HEAD': self.head,
             'OPTIONS': self.options
         }
+        self.headers = default_headers or {}
 
-    def __call__(self, method, request: Request, *args, **kwargs):
-        self._map_methods[method](request, *args, **kwargs)
+    def __call__(self, method, request: Request, *args, **kwargs) -> Response:
+        response, status_code = self._map_methods[method](request, *args, **kwargs)
+        if type(response) == Response:
+            return response
+        if status_code:
+            return Response(status_code, self.headers, response)
+        return Response(200, self.headers, None)
 
     def get(self, request: Request, *args, **kwargs) -> Response:
         raise MethodNotAllowedError(HttpMethods.get)
