b39e93331c6d2879c764aceb0513f99b5d1ec6fa
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2025-01-15 10:48:06 -0300

    fix: remove default values at Response class

--- pebarest/models/response.py
+++ pebarest/models/response.py
@@ -1,8 +1,15 @@
+from typing import Optional, Union
+
+
 class Response:
-    def __init__(self):
-        self.status = 200
-        self.headers = {}
-        self.body = {}
+    status: int
+    headers: dict
+    body: Optional[dict, str]
+
+    def __init__(self, status: int, headers: dict, body: Union[dict, str]=None):
+        self.status = status
+        self.headers = headers
+        self.body = body
 
 
 __all__ = ['Response']
