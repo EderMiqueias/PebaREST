d30d591f6b1440432affcf752c1ef3e2e1044ed2
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2025-01-17 17:02:42 -0300

    fix: separates default headers from custom headers

--- pebarest/models/request.py
+++ pebarest/models/request.py
@@ -2,25 +2,40 @@ from typing import Optional
 from urllib.parse import parse_qs
 
 
+DEFAULT_HEADERS = [
+    'ACCEPT',
+    'USER-AGENT',
+    'REQUEST-START-TIME',
+    'ACCEPT-ENCODING',
+    'HOST',
+    'CONNECTION'
+]
+
+
 class Request:
     headers: dict
+    _headers: dict
     params: dict
     body: Optional[dict]
 
     def __init__(self, environ):
-        self.headers = self.parse_headers(environ)
+        self.headers, self._headers = self.parse_headers(environ)
         self.params = self.parse_params(environ)
         self.body = {}
 
     @staticmethod
     def parse_headers(environ):
         """Converts the request headers into a dictionary."""
-        headers = {}
+        custom_headers = {}
+        default_headers = {}
         for key, value in environ.items():
             if key.startswith('HTTP_'):
-                header_name = key[5:].replace('_', '-').title()
-                headers[header_name] = value
-        return headers
+                header_name = key[5:].replace('_', '-')
+                if header_name in DEFAULT_HEADERS:
+                    default_headers[header_name.title()] = value
+                else:
+                    custom_headers[header_name.title()] = value
+        return custom_headers, default_headers
 
     @staticmethod
     def parse_params(environ):
