86dfe2f78af424fe7fc4462de889a71762892aea
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2025-03-05 11:04:40 -0300

    chore: remove unused methods

--- pebarest/models/base_model.py
+++ pebarest/models/base_model.py
@@ -51,32 +51,6 @@ class BaseModel(JsonClass):
         return self.to_dict()
 
     @staticmethod
-    def _get_internal_attrs_keys() -> tuple:
-        return '_BaseModel__attrs', '_BaseModel__sub_instances'
-
-    @staticmethod
-    def __type_is_subclass_of_class_tuple(class_type, union_class_type):
-        for ct_item in get_args(union_class_type):
-            if issubclass(ct_item, class_type):
-                return True
-        return False
-
-    @staticmethod
-    def __check_item(item) -> bool:
-        try:
-            if get_origin(item[1]) is Union:
-                return BaseModel.__type_is_subclass_of_class_tuple(BaseModel, item[1])
-            return issubclass(item[1], BaseModel)
-        except AttrTypeError:
-            return False
-
-    @staticmethod
-    def __get_attrs_instance(cls) -> dict:
-        return dict(
-            filter(BaseModel.__check_item, cls.__annotations__.items())
-        )
-
-    @staticmethod
     def __check_attr_is_optional(cls, attr_name) -> bool:
         attr_class_type = cls.__annotations__[attr_name]
         if get_origin(attr_class_type) is Union:
