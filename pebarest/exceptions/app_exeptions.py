8ce2e613807e305140c0b5caea5fd13e33038d4c
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2025-04-21 10:30:12 -0300

    feat: add NotFoundError exception class

--- pebarest/exceptions/app_exeptions.py
+++ pebarest/exceptions/app_exeptions.py
@@ -11,3 +11,12 @@ class MethodNotAllowedError(Exception):
     def __init__(self, method: str):
         self.method = method
         self.title = f'405 Method Not Allowed'
+
+
+class NotFoundError(Exception):
+    message: str
+    status_code: int
+
+    def __init__(self, message: str='Resource not found', status_code: int=404):
+        self.message = message
+        self.status_code = status_code
