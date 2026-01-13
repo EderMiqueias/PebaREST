827cd5156b4a5f8cc26053831fcda4d573edcb6e
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2026-01-13 01:09:31 -0300

    add PebaColoredFormatter to logging as default formatter

--- pebarest/utils/logging.py
+++ pebarest/utils/logging.py
@@ -1,6 +1,35 @@
 import logging
 
+
+class PebaColoredFormatter(logging.Formatter):
+    """Formatador com cores ANSI para o terminal."""
+
+    grey = "\x1b[38;20m"
+    green = "\x1b[32;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+
+    # O formato base: Data | Nivel | Modulo | Mensagem
+    format_str = "[%(asctime)s] [%(levelname)-8s] %(name)s: %(message)s"
+
+    FORMATS = {
+        logging.DEBUG: grey + format_str + reset,
+        logging.INFO: green + format_str + reset,
+        logging.WARNING: yellow + format_str + reset,
+        logging.ERROR: red + format_str + reset,
+        logging.CRITICAL: bold_red + format_str + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt, datefmt='%Y-%m-%d %H:%M:%S')
+        return formatter.format(record)
+
+
 default_handler = logging.StreamHandler()
+default_handler.setFormatter(PebaColoredFormatter())
 
 def has_level_handler(logger: logging.Logger) -> bool:
     level = logger.getEffectiveLevel()
