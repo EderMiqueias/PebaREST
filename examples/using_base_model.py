c6eaa10e1b32d04402372648e779da809a0c27c6
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2026-01-23 00:12:43 -0300

    upgrade using_base_model.py example

--- examples/using_base_model.py
+++ examples/using_base_model.py
@@ -1,11 +1,28 @@
-from typing import Optional
-from pebarest import BaseModel
+from wsgiref.simple_server import make_server
+
+from pebarest import BaseModel, App
+from pebarest.models import Resource, Request
 
 
 class Item(BaseModel):
     name: str
     quantity: int
-    description: Optional[str] = None
 
 
+# Class for simple resource
+class GreetingResource(Resource):
+    def get(self, request: Request[Item], *args, **kwargs):
+        print(request.body)
+        print(request.params)
+        return {"message": "Hello my little peba!"}
 
+
+app = App(__name__, default_headers={'Content-Type': 'application/json'})
+
+# Add a new route path
+app.add_route('/greeting', GreetingResource())
+
+# Start the server
+with make_server('', 8000, app) as httpd:
+    print("Server listening on http://127.0.0.1:8000")
+    httpd.serve_forever()
